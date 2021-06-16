fd1b18adebef9f3296901d8ca75d2e64a9946317
vikrant-ku <vikrant.rohartech@gmail.com>
2021-06-16 12:20:05 +0530

    update prod Allow Hosts

--- mysite/mysite/settings/prod.py
+++ mysite/mysite/settings/prod.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['3.6.238.39']
+ALLOWED_HOSTS = ['rohartech.com', 'www.rohartech.com']
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
